51b824b6553fe2a2fdf268a713399e2aca011757
shuoting kao <tim.kao@tagnology.co>
2021-09-18 19:26:23 -0400

    upload first version

---
+++ Skeleton/Gameboard.py
@@ -0,0 +1,68 @@
+import db
+
+class Gameboard():
+    def __init__(self):
+        self.player1 = ""
+        self.player2 = ""
+        self.rows = rows = 6
+        self.cols = cols = 7
+        self.board = [['' for x in range(cols)] for y in range(rows)]
+        self.game_result = ""
+        self.current_turn = 'p1'
+        self.emptyRowAtCol = [self.rows - 1] * cols
+
+    def clean(self):
+        self.__init__()
+
+
+    def judge(self, r, c): # check if anyone wins
+        left = right = c
+        board = self.board
+        player = board[r][c]
+        while left - 1 >= 0 and board[r][left - 1] == player:
+            left -= 1
+        while right + 1 < self.cols and board[r][right + 1] == player:
+            right += 1
+        if right - left >= 3:
+            return True
+        up = down = r
+        while up - 1 >= 0 and board[up - 1][c] == player:
+            up -= 1
+        while down + 1 < self.rows and board[down + 1][c] == player:
+            down += 1
+        return down - up >= 3
+
+    def isReady(self):
+        return (self.player1 and self.player2), 'Color not selected'
+
+    def isFinish(self):
+        return self.game_result != ""
+
+
+    def switch(self):
+        self.current_turn = 'p1' if self.current_turn == 'p2' else  'p2'
+
+
+    def move(self, col, player):
+        row = self.emptyRowAtCol[col]
+        if row == -1:
+            return False, 'No space left at this column'
+        if self.isFinish():
+            print("ADF")
+            return False, 'Game over'
+        if player != self.current_turn:
+            return False, 'Not your turn'
+        self.emptyRowAtCol[col] -= 1 # decrease row
+        self.board[row][col] = self.player1 if self.current_turn == 'p1' else self.player2
+        if self.judge(row, col):
+            self.game_result = 'player1' if self.current_turn == 'p1' else 'player2'
+        self.switch()
+        return True, ''
+
+
+'''
+Add Helper functions as needed to handle moves and update board and turns
+'''
+
+
+    
---
+++ Skeleton/app.py
@@ -0,0 +1,124 @@
+from flask import Flask, render_template, request, redirect, jsonify
+from json import dump
+
+from flask.globals import g, session
+from Gameboard import Gameboard
+import db
+
+
+app = Flask(__name__)
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+global game
+game = None
+'''
+Implement '/' endpoint
+Method Type: GET
+return: template player1_connect.html and status = "Pick a Color."
+Initial Webpage where gameboard is initialized
+'''
+
+
+@app.route('/', methods=['GET'])
+def player1_connect():
+    global game
+    game = Gameboard()
+    game.clean()
+    return render_template('player1_connect.html', status="Pick a Color.")
+
+
+'''
+Helper function that sends to all boards don't modify
+'''
+
+
+@app.route('/autoUpdate', methods=['GET'])
+def updateAllBoards():
+    try:
+        return jsonify(move=game.board, winner=game.game_result,
+                       color=game.player1)
+    except Exception:
+        return jsonify(move="")
+
+
+'''
+Implement '/p1Color' endpoint
+Method Type: GET
+return: template player1_connect.html and status = <Color picked>
+Assign player1 their color
+'''
+
+
+@app.route('/p1Color/', methods=['GET'])
+def player1_config():
+    global game
+    game.player1 = request.args.get('color')
+    return render_template('player1_connect.html', status="Color picked " + game.player1)
+
+
+'''
+Implement '/p2Join' endpoint
+Method Type: GET
+return: template p2Join.html and status = <Color picked> or Error
+if P1 didn't pick color first
+
+Assign player2 their color
+'''
+
+
+@app.route('/p2Join', methods=['GET'])
+def p2Join():
+    res = "Error"
+    if game.player1 and game.player1 in ['red', 'yellow']:
+        res = game.player2 = 'yellow' if game.player1 == 'red' else 'red'
+    return render_template('p2Join.html', status=res)
+
+
+'''
+Implement '/move1' endpoint
+Method Type: POST
+return: jsonify (move=<CurrentBoard>,
+invalid=True or False, winner = <currWinner>)
+If move is valid --> invalid = False else invalid = True
+If invalid == True, also return reason= <Why Move is Invalid>
+
+Process Player 1's move
+'''
+
+
+@app.route('/move1', methods=['POST'])
+def p1_move():
+    try:
+        rdy, reasons = game.isReady()
+        if not rdy:
+            return jsonify(move=game.board, invalid=not rdy, reason=reasons,winner=game.game_result)
+        jsonObj = request.get_json()
+        col = int(jsonObj['column'][3:]) - 1 # transform it to 0-index
+        valid, reasons = game.move(col, 'p1')
+        return jsonify(move=game.board, invalid=not valid, reason=reasons,winner=game.game_result)
+    except:
+        return jsonify(move=game.board, invalid=True, reason='unexpected error', winner=game.game_result)
+
+'''
+Same as '/move1' but instead proccess Player 2
+'''
+
+
+@app.route('/move2', methods=['POST'])
+def p2_move():
+    try:
+        rdy, reasons = game.isReady()
+        if not rdy:
+            return jsonify(move=game.board, invalid=not rdy, reason=reasons,winner=game.game_result)
+        jsonObj = request.get_json()
+        col = int(jsonObj['column'][3:]) - 1 # transform it to 0-index
+        valid, reasons = game.move(col, 'p2')
+        return jsonify(move=game.board, invalid=not valid, reason=reasons,winner=game.game_result)
+    except:
+        return jsonify(move=game.board, invalid=True, reason='unexpected error', winner=game.game_result)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.run(debug=True, host='127.0.0.1')
